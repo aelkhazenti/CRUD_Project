0250ec31cdea37f67a029d0b096b08d6922c5fb6
mac <aelkhazenti@gmail.com>
2019-12-09 21:43:27 +0100

    database template

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,22 +1,87 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import swal from 'sweetalert';
+import style from '../../css/index.css';
 
 export default class Example extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
 
-                            <div className="card-body">I'm an example component!</div>
+
+    render() {
+
+        return (
+
+
+            <div className="container">
+
+                <body className="align">
+                <h1> Connect to DATABASE </h1>
+
+                <div className="grid">
+                    <form className="form login">
+
+
+                        <div className="form__field">
+                            <label htmlFor="Host">
+                                <svg className="icon">
+                                    <a uk-icon="server"></a>
+                                </svg>
+                                <span className="hidden">Host adress </span></label>
+                            <input id="Host" type="text" name="Host" className="form__input"
+                                   placeholder="Host adress" required />
                         </div>
-                    </div>
+
+                        <div className="form__field">
+                            <label htmlFor="database">
+                                <svg className="icon">
+                                    <a uk-icon="database"></a>
+                                </svg>
+                                <span className="hidden">database name </span></label>
+                            <input id="database" type="text" name="database" className="form__input"
+                                   placeholder="database name" required />
+                        </div>
+
+                        <div className="form__field">
+                            <label htmlFor="user">
+                                <svg className="icon">
+                                    <a uk-icon="user"></a>
+
+                                </svg>
+                                <span className="hidden">user</span></label>
+                            <input id="user" type="text" name="user" className="form__input"
+                                   placeholder="user" required />
+                        </div>
+
+                        <div className="form__field">
+                            <label htmlFor="password">
+                                <svg className="icon">
+                                    <a uk-icon="lock"></a>
+                                </svg>
+                                <span className="hidden">password</span></label>
+                            <input id="password" type="password" name="password" className="form__input"
+                                   placeholder="password" required />
+                        </div>
+
+
+
+
+                        <div className="form__field">
+                            <input type="submit" value="Connect" />
+                        </div>
+
+                    </form>
+
+
+
                 </div>
+                </body>
+
+
             </div>
+
         );
     }
+
+
 }
 
 if (document.getElementById('example')) {
